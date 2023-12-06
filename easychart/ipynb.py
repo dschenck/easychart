5fe1076bdc97c89a41a4e0ab7e56b953582e41e5
David Schenck <david.schenck@outlook.com>
2023-12-06 22:27:48 +0100

    set iFrame min height to 400px

--- easychart/ipynb.py
+++ easychart/ipynb.py
@@ -49,7 +49,7 @@ if ip and (
         return f"""
             <iframe 
                 style="border:0;outline:none;width:{easychart.internals.Size(grid.width or easychart.config.rendering.container.width)};max-width:{easychart.internals.Size(easychart.config.rendering.container.get("max-width", "100%"))}" 
-                onload='javascript:(function(o){{o.style.height=o.contentWindow.document.body.scrollHeight+"px";}}(this));' 
+                onload='javascript:(function(o){{o.style.height=Math.max(400, o.contentWindow.document.body.scrollHeight)+"px";}}(this));' 
                 srcdoc="{html.escape(template)}">
             </iframe>
         """
