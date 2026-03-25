e8ba8c118506f32eabf1caefd21ead42a6404265
David Schenck <david.schenck@outlook.com>
2026-03-25 22:03:21 +0100

    feat: annotate and test deduplicate function

--- easychart/utils.py
+++ easychart/utils.py
@@ -1,3 +1,21 @@
-def deduplicate(lst):
-    seen = set()
+from typing import Iterable, List, TypeVar, Hashable
+
+T = TypeVar("T", bound=Hashable)
+
+
+def deduplicate(lst: Iterable[T]) -> List[T]:
+    """
+    Return a deduplicated list of items, in the order of the
+    original list
+
+    Parameters
+    ----------
+    lst : iterable
+        Iterable of hashable values
+
+    Returns
+    -------
+    list
+    """
+    seen: set[T] = set()
     return [x for x in lst if not (x in seen or seen.add(x))]
